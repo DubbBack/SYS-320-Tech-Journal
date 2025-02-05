81f52211c243b2ce343801b0d903bc6f98bda0b3
Colin <colin.woodrow@mymail.champlain.edu>
2025-02-05 14:06:34 -0500

    week 4

---
+++ week4/dotNotation.ps1
@@ -0,0 +1,9 @@
+. (Join-Path $PSScriptRoot eventLogs.ps1) 
+. (Join-Path $PSScriptRoot startShutDown.ps1)
+
+clear
+
+Get-Content "C:\Users\champuser\SYS-320-Tech-Journal\week4\dotNotation.ps1"
+
+$loginoutsTable 
+$startShutdownResult 
