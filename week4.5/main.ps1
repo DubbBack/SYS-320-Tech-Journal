634794f0130c85de5ac01c48a7e417e0d5cd1646
Colin <colin.woodrow@mymail.champlain.edu>
2025-02-12 15:04:40 -0500

    Parsing Apache Logs

--- week4.5/main.ps1
+++ week4.5/main.ps1
@@ -1,6 +1,6 @@
 # Import the Apache-Logs.ps1 script
 . .\Apache-Logs.ps1
-
+. .\parseLogs.ps1
 # Define inputs
 $page = "http://10.0.17.26/index.html"   # Change this to the desired page
 $httpCode = "404"      # Change this to the desired HTTP response code
@@ -12,3 +12,5 @@ $results = Get-ApacheLogIPs -Page $page -HttpCode $httpCode -Browser $browser
 # Display results
 $results | Format-Table -AutoSize
 
+$newResults = ApacheLogs1 
+$newResults | Format-Table -AutoSize -Wrap
