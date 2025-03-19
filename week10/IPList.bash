0c362716c7b0d6d6952498e8c66ca8a9644ea4c6
Colin <colin.woodrow@mymail.champlain.edu>
2025-03-19 14:50:02 -0400

    Week 10 IP listing

---
+++ week10/IPList.bash
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# List all the ips inn the given network prefix
+# /24 only
+
+#Usage: bash IPList.bash 10.0.17
+if [ $# -ne 1 ] ; then
+    echo "Usage: $0 <Prefix>"
+    exit 1
+fi
+
+prefix=$1
+
+if [ ${#prefix} -lt 6 ] ; then
+	printf "Prefix length is to short\nPrefix example: 10.0.17\n" ;
+	exit 1
+fi
+
+
+for i in {1..20}
+do
+	ping -c 1 $prefix.$i > /dev/null 2>&1
+	if [ $? -eq 0 ]; then
+		result=0
+	else
+		result=1
+	fi
+	if [ $result -eq 0 ]; then
+	   echo "$prefix.$i"
+	fi
+done
